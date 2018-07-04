e92bba4c9a064ed817849236ea733ca7a43f7d45
Sirgaden <smarquez.dev@gmail.com>
2018-07-04 13:32:03 +0200

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-// Routes
+// * Routes
 import { APP_ROUTING } from './app.routes';
 
+// * Modules
+import { PagesModule } from './pages/pages.module';
+
+// * Components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTING
+    APP_ROUTING,
+    PagesModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,30 +9,10 @@ import { ProgressComponent } from './pages/progress/progress.component';
 import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { RegisterComponent } from './login/register.component';
 
-// const APP_ROUTES: Routes = [
-//   { path: '', component: PagesComponent },
-//   { path: 'dashboard', component: DashboardComponent },
-//   { path: 'login', component: LoginComponent },
-//   { path: 'register', component: DashboardComponent },
-//   { path: 'progress', component: ProgressComponent },
-//   { path: 'graficas1', component: Graficas1Component },
-//   // { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
-//   { path: '**', component: NopagefoundComponent }
-// ];
-
 const APP_ROUTES: Routes = [
-  { path: '',
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'graficas1', component: Graficas1Component },
-      { path: '', pathMatch: 'full', redirectTo: '/dashboard' }
-    ]
-  },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '**', component: NopagefoundComponent }
 ];
 
-export const APP_ROUTING = RouterModule.forRoot( APP_ROUTES, { useHash: true } );
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, { useHash: true });
